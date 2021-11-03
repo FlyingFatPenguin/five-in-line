1f8979140e45643d1a764b10b0e9477f60775a81
FlyingFatPenguin <justAUselessEmail@163.com>
2021-11-03 23:52:13 +0800

    feat: 实现游戏基本流程

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,7 +1,8 @@
 import { useCallback, useEffect, useState } from "react";
 import Board from "../components/Board";
 import { GameBoard } from '../game';
-import { Pos } from "../util/pos";
+import { randomInt } from "../util/math";
+import { eqPos, Pos } from "../util/pos";
 
 const style: React.CSSProperties = {
     height: '100vh',
@@ -22,26 +23,36 @@ interface Props {
 
 const MainPage: React.FC<Props> = props => {
     const update = useUpdate()
+    const [level] = useState(5);
     const [game] = useState(() => new GameBoard())
-    const [pos, setPos] = useState<Pos>()
+    const [activePos, setPos] = useState<Pos>()
+
+    const newBlock = useCallback(() => {
+        return randomInt(level)
+    }, [level])
 
     useEffect(() => {
-        game.randomPickEmpty(5)?.forEach(p => game.add(p, 2))
+        game.randomPickEmpty(5)?.forEach(p => game.add(p, newBlock()))
         update()
-    }, [game, update])
+    }, [game, newBlock, update])
+
+    const move = useCallback((f: Pos, t: Pos) => {
+        game.move(f, t);
+        game.randomPickEmpty(3)?.forEach(p => game.add(p, newBlock()))
+        update()
+    }, [game, newBlock, update])
 
     const handleClick = useCallback((p: Pos) => {
-        if (pos) {
+        if (activePos && !eqPos(p, activePos)) {
             setPos(undefined)
-            game.move(pos, p);
-            update()
+            move(activePos, p)
             return;
         }
         setPos(p)
-    }, [pos])
+    }, [move, activePos])
 
     return <div style={style}>
-        <Board data={game.clone()} onClick={handleClick} activePos={pos} />
+        <Board data={game.clone()} onClick={handleClick} activePos={activePos} />
     </div>;
 }
 
