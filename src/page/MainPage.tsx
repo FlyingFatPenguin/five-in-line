401b031ecf92149f76ec2cbdc9a6d7c39ba40438
FlyingFatPenguin <justAUselessEmail@163.com>
2021-11-05 23:50:40 +0800

    feat: 实现移动过程

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,8 +1,11 @@
+import { useBoolean } from "@huse/boolean";
 import { useCallback, useEffect, useState } from "react";
 import Board from "../components/Board";
 import { GameBoard } from '../game';
 import { randomInt } from "../util/math";
 import { eqPos, Pos } from "../util/pos";
+
+const sleep = (ms: number) => new Promise(res => setTimeout(res, ms));
 
 const style: React.CSSProperties = {
     height: '100vh',
@@ -36,13 +39,30 @@ const MainPage: React.FC<Props> = props => {
         update()
     }, [game, newBlock, update])
 
-    const move = useCallback((f: Pos, t: Pos) => {
-        game.move(f, t);
+    const [isMoving, { on, off }] = useBoolean(false);
+
+    const move = useCallback(async (f: Pos, t: Pos) => {
+        const way = game.findWay(f, t);
+        if (!way) {
+            return
+        }
+        on()
+        let last = f;
+        for (const p of way) {
+            await sleep(50);
+            game.move(last, p);
+            last = p
+            update()
+        }
         game.randomPickEmpty(3)?.forEach(p => game.add(p, newBlock()))
         update()
-    }, [game, newBlock, update])
+        off()
+    }, [game, newBlock, off, on, update])
 
     const handleClick = useCallback((p: Pos) => {
+        if (isMoving) {
+            return
+        }
         if (activePos) {
             if (game.canMove(activePos, p)) {
                 move(activePos, p)
@@ -51,7 +71,7 @@ const MainPage: React.FC<Props> = props => {
             return;
         }
         setActivePos(p)
-    }, [activePos, game, move])
+    }, [activePos, game, isMoving, move])
 
     return <div style={style}>
         <Board data={game.clone()} onClick={handleClick} activePos={activePos} />
