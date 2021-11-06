fddff31f0f73132d742f91d240e8143b45224728
FlyingFatPenguin <justAUselessEmail@163.com>
2021-11-06 19:04:30 +0800

    feat: 提示下一次新增动物

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,5 +1,7 @@
 import { useBoolean } from "@huse/boolean";
+import { range } from "lodash";
 import { useCallback, useEffect, useState } from "react";
+import Block from "../components/block/Block";
 import Board from "../components/Board";
 import { GameBoard } from '../game';
 import { randomInt } from "../util/math";
@@ -30,14 +32,21 @@ const MainPage: React.FC<Props> = props => {
     const [game] = useState(() => new GameBoard())
     const [activePos, setActivePos] = useState<Pos>()
     const [score, setScore] = useState(0);
+    const [nextColor, setNextColor] = useState<number[]>();
 
-    const newBlock = useCallback(() => {
+    const randomColor = useCallback(() => {
         const level = Math.min(Math.floor(Math.max((score - 500), 0) / 500 + 5), 9);
         return randomInt(level - 1) + 1
     }, [score])
 
+    const addThree = useCallback(() => {
+        game.randomPickEmpty(3)?.forEach((p, i) => game.add(p, nextColor ? nextColor[i] : randomColor()))
+        setNextColor(range(3).map(randomColor))
+    }, [game, nextColor, randomColor])
+
     useEffect(() => {
-        game.randomPickEmpty(5)?.forEach(p => game.add(p, newBlock()))
+        game.randomPickEmpty(2)?.forEach(p => game.add(p, randomColor()))
+        addThree()
         update()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [game])
@@ -59,13 +68,13 @@ const MainPage: React.FC<Props> = props => {
         }
         const clearNum = game.clearInLine()
         if (!clearNum) {
-            game.randomPickEmpty(3)?.forEach(p => game.add(p, newBlock()))
+            addThree()
         }
         console.log(clearNum)
         setScore(v => v + clearNum)
         update()
         off()
-    }, [game, newBlock, off, on, update])
+    }, [game, on, update, off, addThree])
 
     const handleClick = useCallback((p: Pos) => {
         if (isMoving) {
@@ -84,6 +93,7 @@ const MainPage: React.FC<Props> = props => {
     return <div style={{ display: 'flex', justifyContent: 'center' }}>
         <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
             <h1>当前分数{score}</h1>
+            {nextColor?.map((v, i) => <Block type={v} key={i} />)}
         </div>
         <div style={style}>
             <Board data={game.clone()} onClick={handleClick} activePos={activePos} />
